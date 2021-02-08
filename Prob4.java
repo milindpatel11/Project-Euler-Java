f647967fdbaa0666fc66c5527b9ad6c2d6d50ba6
Milind <milind@Milinds-MacBook-Air.local>
2021-02-08 10:29:22 +0530

    Prob4 another way of calculating added

--- Prob4.java
+++ Prob4.java
@@ -26,6 +26,9 @@ public class Prob4 {
   public static void main (String args[]) {
 
 
+    // Naive brute method - without duplicates
+    //
+
     int max_peli = 0;
 
     for (int i = 999 ; i > 99 ; i -- ) {
@@ -39,6 +42,46 @@ public class Prob4 {
     }
 
     System.out.println(max_peli);
+
+    //  Little smarter brute method - without duplicates
+    //
+
+    max_peli = 0;
+    int temp_a = 1;
+    int temp_b = 1;
+
+    // First try to get first peli
+    for (int i = 999 ; i > 99 ; i -- ) {
+      for (int j = i ; j > 99 ; j -- ) {
+
+        int prod = i * j;
+
+        if ( isPalindrome(prod)) {
+          max_peli = prod;
+          temp_a = i;
+          temp_b = j;
+          break;
+        };
+
+      }
+    }
+
+    // Second try to limit loop to numbers above min of a and break;
+
+    for (int i = 999 ; i >= temp_a ; i -- ) {
+      for (int j = i ; j >= temp_b ; j -- ) {
+
+        int prod = i * j;
+
+        if ( isPalindrome(prod) && prod > max_peli ) max_peli = prod;
+
+      }
+    }
+
+
+    System.out.println(max_peli);
+
+
 
   }
 
